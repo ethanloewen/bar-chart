db88cc774c076635cc0237441731de49f9e8a9f2
Ethan Loewen <ethanloewen@gmail.com>
2022-02-04 23:06:04 +0000

    Added a basic labels and changed data to a 2D array as a result

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 //Data is the array that will be graphed
-let data = [1, 2, 3, 4, 5, 10, 2, 5.5];
+let data = [
+  [1, 'label1'],
+  [2, 'label2'],
+  [3, 'label3'],
+  [4, 'label4'],
+  [5, 'label5']
+];
 let options = {
   width: '100%',
   height: '800px',
   barColor: '#FFFDD0',
-  labelColor: '',
+  labelColor: 'black',
   spacing: '',
   axes: '',
   valuesPosition: 'top' //'top', 'centre', or 'bottom'
@@ -23,6 +29,8 @@ function drawBarChart(data, options, element) {
 function setDimensions() {
   $(element).css("width", options.width);
   $(element).css("height", options.height);
+  //Keeps the label width the same as the background width
+  $("#label").css("width", options.width);
 }
 
 
@@ -38,9 +46,12 @@ function setOptions() {
     $('.bars p').css('top', '50%');
   }
   else if(options.valuesPosition === 'bottom') {
-    //STILL NEED TO ADD
     $('.bars p').css('bottom', '0');
   }
+
+  //Sets the data's color
+  $('.bars p').css('color', options.labelColor);
+
 }
 
 
@@ -53,23 +64,27 @@ function makeElements() {
     //Appends a new span to the element (declared above) with a class of 'bars' and a unique id of 'bar' + i
     $(element).append($("<span class='bars' id='bar" + i + "'></span>"));
     //Appends a new paragraph to the span we just created
-    $("#bar" + i).append($("<p>" + data[i] + "</p>"));
+    $("#bar" + i).append($("<p>" + data[i][0] + "</p>"));
 
     //Sets a height for the bar based on the largest data entry
-    barHeight = (data[i] / largestElement) * 100;
+    barHeight = (data[i][0] / largestElement) * 100;
     $('#bar' + i).css('height', (barHeight + '%'));
+
+
   }
   //change the bars width
-  $('.bars').css("width", '50px');
+  //$('.bars').css("width", '75px');
 }
+
+
 
 //Returns the largest value from the data
 function getLargest() {
   let largest = 0;
   //Iterate through the data array and compare the current element with the largest saved number
   for(let i = 0; i < data.length; i++) {
-    if(data[i] > largest) {
-      largest = data[i];
+    if(data[i][0] > largest) {
+      largest = data[i][0];
     }
   }
   return largest;
